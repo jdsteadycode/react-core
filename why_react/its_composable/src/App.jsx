1027baec2112b3e29354a4d32586f22b3c365201
jdsteadycode <desaijeet11@gmail.com>
2025-08-05 17:46:59 +0530

    added App Component for react-project

---
+++ why_react/its_composable/src/App.jsx
@@ -0,0 +1,13 @@
+// () -> App Component
+function App() {
+
+  return (
+    <>
+      <main>
+        A custom Navbar Component built using bootstrap
+      </main>
+    </>
+  )
+}
+
+export default App
